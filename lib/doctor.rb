38357e9f8f6cbf3f431ab237fc4469cfe53e63f6
Maxine <maxinemeurer@yahoo.com>
2021-02-18 21:29:14 -0500

    commit tree c79dcecfb625159231f0433cbcbf9f7c7872b358

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,25 @@
+class Doctor
+    attr_reader :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+      end
+
+    def appointments
+        Appointment.all {|appointment| appointment.doctor == self}
+    end
+
+    def patients
+        appointments.map(&:patient)
+    end
+end
--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,18 @@
+class Song
+    @@all = []
+    attr_accessor :name
+    attr_reader :artist, :genre
 
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+
+end
